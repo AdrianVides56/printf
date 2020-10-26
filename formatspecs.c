3759874fd4267b4eec12f880f07982bdc381cc33
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2020-10-26 19:01:05 +0000

    Added function print_num for print a number

--- formatspecs.c
+++ formatspecs.c
@@ -41,3 +41,37 @@ int print_p(va_list __attribute__((unused)) list)
 	_putchar(37);
 	return (1);
 }
+
+/**
+ * print_num - Print a number > 0 or < 0.
+ * @list:variable to an arguments list.
+ * Return: Length of printed number
+ */
+int print_num(va_list list)
+{
+	unsigned int decena, unidad;
+	int num = va_arg(list, int), len = 0;
+
+	if (num < 0)
+	{
+		_putchar('-');
+		num = num * -1; /* Si el numero es menor a 0 cambia el signo*/
+		len++;
+	}
+	decena = 1;
+	unidad = num;
+
+	while (unidad > 9)
+	{
+		decena *= 10;
+		unidad /= 10;
+	}
+
+	for (; decena >= 1; decena /= 10)
+	{
+		_putchar(((num / decena) % 10) + '0');
+		len++;
+	}
+
+	return (len);
+}
