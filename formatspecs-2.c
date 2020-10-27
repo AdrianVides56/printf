60540fe7f4f636f71d0874e57f8a7a765361687a
AdrianVides56 <adrianvj18@gmail.com>
2020-10-27 11:41:01 -0500

    Created print_b function

---
+++ formatspecs-2.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * print_b - convert a positive number to binary and prints it
+ * @list: variable to an arguments list
+ * Return: number of bytes
+ */
+int print_b(va_list list)
+{
+	int mod[32], dividendo, i = 0, x;
+
+	dividendo = va_arg(list, int);
+	if (dividendo == 0)
+	{
+		_putchar(48);
+		_putchar(10);
+		return (1);
+	}
+	if (dividendo < 0)
+		return (0);
+
+	while (dividendo > 0)
+	{
+		mod[i] = dividendo % 2;
+		dividendo = dividendo / 2;
+		i++;
+	}
+
+	for (x = i - 1; x >= 0; x--)
+	{
+		_putchar(mod[x] + '0');
+	}
+	_putchar(10);
+	return (i);
+}
